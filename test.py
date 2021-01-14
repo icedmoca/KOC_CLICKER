426898f8f9629b4b1dd2050c9e290c65cfea79ce
Kyle Drake <blazehavenservers@gmail.com>
2021-01-14 01:29:03 -0700

    Update test.py

--- test.py
+++ test.py
@@ -1,42 +1,63 @@
-from seleniumwire import webdriver  # Import from seleniumwire
+from seleniumwire import webdriver
+from selenium.webdriver.chrome.options import Options
 import time
-# Create a new instance of the Firefox driver
-chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument('--headless')
-chrome_options.add_argument('--no-sandbox')
-driver = webdriver.Chrome(options=chrome_options)
-driver.get('https://kingoftheclicks.com/?ref=epicgamer')
+import json
+from random import randint
+packetnum = False
 
 def interceptor(request):
-    if request.method == 'POST' and request.headers['Content-Type'] == 'application/json':
-        # The body is in bytes so convert to a string
-        print('starting injection')
-        body = request.body.decode('utf-8')
-        # Load the JSON
-        data = json.loads(body)
-        # Add a new property
-        print(data)
-        #data[2] = '"number":200'
-        # Set the JSON back on the request
-        request.body = json.dumps(data).encode('utf-8')
-        # Update the content length
-        del request.headers['Content-Length']
-        request.headers['Content-Length'] = str(len(request.body))
-
-# Access requests via the `requests` attribute
-time.sleep(6)
-start = driver.find_element_by_xpath('/html/body/div[1]/div/div/main/div[3]/div[2]/div/div/div/footer/button[1]/span')
-start.click()
-time.sleep(1)
-print('STARTING')
-removeButt = driver.find_element_by_xpath('/html/body/div[1]/div/div/main/div[3]/div[3]/div/div[2]/div/div[2]/div[2]/div[2]/div[2]/span/span')
-driver.request_interceptor = interceptor
-
-        #for x in range(1):
-removeButt.click()
-pRequest = driver.wait_for_request('https://gobo66g2oj.execute-api.us-east-1.amazonaws.com/dev/modify', timeout=10)
-try:
-    interceptor(pRequest)
-except:
-    print('didnt work lol get good')
-    pass
+    try:
+        global packetnum
+        if request.method == 'POST' and request.headers['Content-Type'] == 'application/json;charset=UTF-8' and request.headers['Host'] == 'gobo66g2oj.execute-api.us-east-1.amazonaws.com':
+            body = request.body.decode('utf-8')
+            if '"number":' in body:
+                try:
+                    print('starting injection')
+                    newnum = str(randint(2,5))
+                    body = body.replace('"number":', ('"number":' + newnum))
+                    print(body)
+                    request.body = body.encode('utf-8')
+                    del request.headers['Content-Length']
+                    request.headers['Content-Length'] = str(len(request.body))
+                    packetnum = True
+                    print('WE DID IT!!!')
+                except:
+                    pass
+    except:
+        pass
+def makeClickerInstance():
+    #try:
+        options = {
+        'backend': 'mitmproxy'
+        }
+        chrome_options = Options()
+        chrome_options.add_argument('--headless')
+        driver = webdriver.Chrome(executable_path=r'chromedriver\chromedriver.exe', seleniumwire_options=options, options=chrome_options)
+        driver.get('https://kingoftheclicks.com/?ref=epicgamer')
+        time.sleep(6)
+        start = driver.find_element_by_xpath('/html/body/div[1]/div/div/main/div[3]/div[2]/div/div/div/footer/button[1]/span')
+        start.click()
+        time.sleep(1)
+        removeButt = driver.find_element_by_xpath('/html/body/div[1]/div/div/main/div[3]/div[3]/div/div[2]/div/div[2]/div[2]/div[2]/div[2]/span/span')
+        global packetnum
+        for x in range(10000):
+            print('starting new instance')
+            packetnum = False
+            driver.request_interceptor = interceptor
+            for a in range(500):
+                removeButt.click()
+            print('buttonclicked')
+            while packetnum == False:
+                try:
+                    interceptor(driver.last_request)
+                except:
+                    pass
+        driver.quit()
+    #except:
+        #print('DRIVER CRASHED RETRYING')
+makeClickerInstance()
+'''
+while 1 == 1:
+    makeClickerInstance()
+    print('RESTARTING')
+'''
