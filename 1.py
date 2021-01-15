66e804c24747001436c1959b79e556e816fdec95
Kade Hugh <77413927+kaderator2@users.noreply.github.com>
2021-01-15 12:11:55 -0700

    Update 1.py

--- 1.py
+++ 1.py
@@ -62,6 +62,6 @@ def makeClickerInstance():
             pass
 
 if __name__ == '__main__':
-    for i in range(10):
+    for i in range(8):
         p = multiprocessing.Process(target=makeClickerInstance)
         p.start()
