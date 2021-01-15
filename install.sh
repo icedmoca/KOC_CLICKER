22ac57770f4a57997f2e11c71c1a7ee534abe7c3
Kyle Drake <blazehavenservers@gmail.com>
2021-01-15 01:03:05 -0700

    uchange

--- install.sh
+++ install.sh
@@ -24,4 +24,4 @@ sudo apt install -y python3-selenium
 pip3 install chromedriver-py
 pip3 install selenium-wire
 pip3 install mitmproxy
-cd /bin & sudo chmod +x chromedriver
+cd /bin && sudo chmod +x chromedriver
