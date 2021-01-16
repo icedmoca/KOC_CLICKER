7a3c159154e7da48d51ab5ff2c992f9229adb810
Kade Hugh <77413927+kaderator2@users.noreply.github.com>
2021-01-15 22:10:05 -0700

    Update test2.py

--- test2.py
+++ test2.py
@@ -1,1 +1,33 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import time
+import multiprocessing
 
+def makeClickerInstance():
+    try:
+        chrome_options = Options()
+        chrome_options.add_argument('--headless')
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--ignore-certificate-errors')
+        chrome_options.add_argument('--disable-web-security')
+        driver = webdriver.Chrome(options=chrome_options)
+        driver.get('https://kingoftheclicks.com/?ref=zodicalpeak')
+        time.sleep(6)
+        start = driver.find_element_by_xpath('/html/body/div[1]/div/div/main/div[3]/div[2]/div/div/div/footer/button[1]/span')
+        start.click()
+        time.sleep(1)
+        removeButt = driver.find_element_by_xpath('/html/body/div[1]/div/div/main/div[3]/div[3]/div/div[2]/div/div[2]/div[2]/div[2]/div[2]/span/span')
+        global packetnum
+        print("Starting clicks")
+        while 1 == 1:
+            for a in range(500):
+                removeButt.click()
+            time.sleep(1)
+    except:
+      pass
+
+if __name__ == '__main__':
+    for i in range(10):
+        p = multiprocessing.Process(target=makeClickerInstance)
+        p.start()
+        time.sleep(5)
