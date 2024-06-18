23e4465678e7a556061fe6a98d3630604192bd43
Kyle Drake <blazehavenservers@gmail.com>
2024-06-18 12:27:10 -0700

    Update 1.py

--- serverclicker/1.py
+++ serverclicker/1.py
@@ -1,5 +1,4 @@
 import time
-
 from seleniumwire import webdriver
 from selenium.webdriver.chrome.options import Options
 from random import randint
@@ -7,69 +6,63 @@ import multiprocessing
 
 packetnum = False
 
-
 def interceptor(request):
+    global packetnum
     try:
-        global packetnum
-        if request.method == 'POST' and request.headers['Content-Type'] == 'application/json;charset=UTF-8' and \
-                request.headers['Host'] == 'gobo66g2oj.execute-api.us-east-1.amazonaws.com':
+        if request.method == 'POST' and request.headers.get('Content-Type') == 'application/json;charset=UTF-8' and \
+                request.headers.get('Host') == 'gobo66g2oj.execute-api.us-east-1.amazonaws.com':
             body = request.body.decode('utf-8')
             if '"number":' in body:
-                try:
-                    newnum = str(randint(170, 180))
-                    body = body.replace('"number":', ('"number":' + newnum))
-                    request.body = body.encode('utf-8')
-                    del request.headers['Content-Length']
-                    request.headers['Content-Length'] = str(len(request.body))
-                    packetnum = True
-                except:
-                    pass
-    except:
-        pass
+                newnum = str(randint(170, 180))
+                body = body.replace('"number":', f'"number":{newnum}')
+                request.body = body.encode('utf-8')
+                request.headers['Content-Length'] = str(len(request.body))
+                packetnum = True
+    except Exception as e:
+        print(f"Error in interceptor: {e}")
 
-
-def makeclickerinstance():
+def make_clicker_instance():
+    global packetnum
     try:
         print("Initializing Instance")
-        options = {
-            'backend': 'mitmproxy'
-        }
         chrome_options = Options()
         chrome_options.add_argument('--headless')
         chrome_options.add_argument('--no-sandbox')
         chrome_options.add_argument('--ignore-certificate-errors')
         chrome_options.add_argument('--disable-web-security')
-        driver = webdriver.Chrome(seleniumwire_options=options, options=chrome_options)
+
+        seleniumwire_options = {'backend': 'mitmproxy'}
+        driver = webdriver.Chrome(seleniumwire_options=seleniumwire_options, options=chrome_options)
+        driver.request_interceptor = interceptor
         driver.get('https://kingoftheclicks.com/?ref=zodicalpeak')
         time.sleep(6)
-        start = driver.find_element_by_xpath(
-            '/html/body/div[1]/div/div/main/div[3]/div[2]/div/div/div/footer/button[1]/span')
-        start.click()
+
+        start_button = driver.find_element_by_xpath('/html/body/div[1]/div/div/main/div[3]/div[2]/div/div/div/footer/button[1]/span')
+        start_button.click()
         time.sleep(1)
-        removeButt = driver.find_element_by_xpath(
-            '/html/body/div[1]/div/div/main/div[3]/div[3]/div/div[2]/div/div[2]/div[2]/div[2]/div[2]/span/span')
-        global packetnum
+        click_button = driver.find_element_by_xpath('/html/body/div[1]/div/div/main/div[3]/div[3]/div/div[2]/div/div[2]/div[2]/div[2]/div[2]/span/span')
+
         print("Starting clicks")
-        for x in range(10000000000):
+        while True:
             packetnum = False
-            driver.request_interceptor = interceptor
-            for a in range(500):  # range(10) = # of clicks at a time
-                removeButt.click()
+            for _ in range(500):  # Number of clicks at a time
+                click_button.click()
             while not packetnum:
-                try:
-                    interceptor(driver.last_request)
-                except:
-                    pass
-        driver.quit()
-    except:
-        print("DriverCrashed restarting...")
+                time.sleep(0.1)
+    except Exception as e:
+        print(f"Driver crashed: {e}")
+    finally:
         try:
             driver.quit()
         except:
             pass
 
-
 if __name__ == '__main__':
-    for i in range(10):  # Number of instances to run
-        p = multiprocessing.Process(target=makeclickerinstance)
+    processes = []
+    for _ in range(10):  # Number of instances to run
+        p = multiprocessing.Process(target=make_clicker_instance)
+        processes.append(p)
         p.start()
+
+    for p in processes:
+        p.join()
